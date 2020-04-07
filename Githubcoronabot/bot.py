37a30d4b50c2dad67e6e74016ef5ca6eb2a67761
AndrewNyrivera <34843760+AndrewNyrivera@users.noreply.github.com>
2020-04-07 17:14:24 -0400

    Add files via upload

---
+++ Githubcoronabot/bot.py
@@ -0,0 +1,73 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+import time
+
+
+def main():
+    x = random.randint(420, 1980)
+    y = random.randint(420, 1980)
+    time1 = random.uniform(0.18, 0.421)
+    time2 = random.uniform(0.12, 0.321)
+    options = Options()
+    ua = UserAgent()
+    userAgent = ua.random
+    print(userAgent)
+    options.add_argument(f'user-agent={userAgent}')
+    driver = webdriver.Chrome(chrome_options=options,
+                              executable_path='chromedriver.exe')
+    driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&switch_account=")
+    driver.set_window_size(x, y)
+    time.sleep(random.randint(1, 2))
+    aemail = driver.find_element_by_name("email")
+    for i in "email":  # <-- just put your amazon email here
+        aemail.send_keys(i)
+        time.sleep(time1)
+
+    time.sleep(random.randint(1, 2))
+    driver.find_element_by_xpath("//*[@id='continue']").click()
+
+    time.sleep(random.randint(1, 2))
+
+    apassword = driver.find_element_by_name("password")
+    for i in "password":  # <-- just put your amazon password here
+        apassword.send_keys(i)
+        time.sleep(time2)
+    time.sleep(random.randint(1, 3))
+    driver.find_element_by_xpath("//*[@id='signInSubmit']").click()
+    driver.implicitly_wait(1000)
+    driver.get(
+        "https://www.amazon.com/gp/cart/view.html?ie=UTF8&ref_=nav_crt_ewc_hd")
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath(
+        "//*[@id='sc-alm-buy-box-ptc-button-VUZHIFdob2xlIEZvb2Rz']/span/input").click()
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath("//*[@id='a-autoid-0']/span/a").click()
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath(
+        "//*[@id='subsContinueButton']/span/input").click()
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath(
+        "//*[@id='changeQuantityFormId']/div[2]/div[2]/div/div/span/span/input").click()
+
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath(
+        "//*[@id='shipoption-select']/div/div/div/div/div[1]/div[3]/div[2]/div/ul/li[2]/span/span/span/button").click()
+    driver.implicitly_wait(1000)
+    driver.find_element_by_xpath(
+        "//*[@id='shipoption-select']/div/div/div/div/div[2]/div[3]/div/span/span/span/input").click()
+
+    while not driver.find_element_by_xpath("//*[@id='shipoption-select']/div/div/div/div/div[2]/div[3]/div/span/span/span/input").click():
+        driver.refresh()
+        time.sleep(random.random(1, 3))
+        if not driver.find_element_by_xpath("//*[@id='shipoption-select']/div/div/div/div/div[2]/div[3]/div/span/span/span/input").click():
+            driver.close()
+            main()
+
+        else:
+            driver.find_element_by_xpath(
+                "//*[@id='shipoption-select']/div/div/div/div/div[2]/div[3]/div/span/span/span/input").click()
+
+
+main()
